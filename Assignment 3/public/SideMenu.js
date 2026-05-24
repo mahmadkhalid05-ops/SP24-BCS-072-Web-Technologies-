c60e3445a1d23926327f65dd67cc2de4ca78ecd9
Muhammad Ahmad <mahmadkhalid05@gmail.com>
2026-05-24 14:19:52 +0500

    push

--- Assignment 3/public/SideMenu.js
+++ Assignment 3/public/SideMenu.js
@@ -1,37 +1,46 @@
-let menuIcon = document.querySelector("#menuButton img");
-let sideMenu = document.getElementById("OpenedMenu");
+window.addEventListener("load", function () {
 
-let isOpen = false;
+    let menuIcon = document.querySelector("#menuButton img");
+    let sideMenu = document.getElementById("OpenedMenu");
 
-menuIcon.addEventListener("click", () => {
-    isOpen = !isOpen;
+    let isOpen = false;
 
-    if (isOpen) {
-        sideMenu.classList.add("active");
-        menuIcon.src = "Body/OpenedMenu/close.99df6797.png";
-    } else {
-        sideMenu.classList.remove("active");
-        menuIcon.src = "Header/Menu/menu.png";
-    }
-});
+    menuIcon.addEventListener("click", () => {
 
-let items = document.querySelectorAll(".MenuGroceryDiv, .MenuElectronicsDiv");
-let groups = document.querySelectorAll(".rightGroup");
-let menuRight = document.getElementById("MenuRight");
+        isOpen = !isOpen;
 
-// show on hover
-items.forEach(item => {
-    item.addEventListener("mouseenter", () => {
-
-        // hide all
-        groups.forEach(g => g.style.display = "none");
-
-        // show target
-        let target = item.getAttribute("data-target");
-        let activeGroup = document.getElementById(target);
-
-        if (activeGroup) {
-            activeGroup.style.display = "grid";
+        if (isOpen) {
+            sideMenu.classList.add("active");
+            menuIcon.src = "Body/Opened Menu/close.99df6797.png";
+        } else {
+            sideMenu.classList.remove("active");
+            menuIcon.src = "Header/Menu/menu.png";
         }
     });
+
+    // ================= MENU HOVER =================
+
+    let items = document.querySelectorAll(".MenuGroceryDiv, .MenuElectronicsDiv");
+    let groups = document.querySelectorAll(".rightGroup");
+    let menuRight = document.getElementById("MenuRight");
+
+    // show on hover
+    items.forEach(item => {
+
+        item.addEventListener("mouseenter", () => {
+
+            // hide all groups
+            groups.forEach(g => g.style.display = "none");
+
+            // show target group
+            let target = item.getAttribute("data-target");
+            let activeGroup = document.getElementById(target);
+
+            if (activeGroup) {
+                activeGroup.style.display = "grid";
+            }
+        });
+
+    });
+
 });
